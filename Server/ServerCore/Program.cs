ee18209543e53fdf8e64e3a1fa1b8144a9716ff9
JiHyeok Heo <vndj1212@gmail.com>
2023-11-14 04:58:46 +0900

    휘발성 키워드 volatile (비추천)

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,44 +6,50 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
         {
-             for(int i =0; i <5; i++)
-                Console.WriteLine("Hellow Thread");
+            Console.WriteLine("쓰레드 시작!");
+
+            // C++ 에서 volatile 키워드는 C#과 조금 다르다
+            // C++ 에서도 코드 최적화 내용도 있다.
+            // C#에서는 volatile 키워드보다 다른 방식을 사용하는걸 권한다.
+            // C#에서는 캐시를 무시하고 최신값을 가져와라 라는 뜻도 있다.
+
+            // 릴리즈 모드로 들어가면 아래와 같이 자동 변환을 해준다. 
+            // 이로 인해 멀티쓰레드 환경에서 오류가 생긴 이유이다.
+            // 이를 해결하기 위한 volatile 이라는 키워드를 사용한다.
+            //if(_stop == false)
+            //{
+            //    while(true)
+            //    {
+
+            //    }
+            //}
+
+            while (_stop == false)
+            {
+                // 누군가가 stop 신호를 해주기를 기다린다.
+            }
+
+            Console.WriteLine("쓰레드 종료!");
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            // 쓰레드 좀 더 효율적으로 관리
-            for(int i =0; i< 5; i++)
-            {
-                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-                t.Start();
-            }
+            // 1초동안 슬립 1000ms
+            Thread.Sleep(1000);
 
-            //for (int i = 0; i < 4; i++)
-            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+            _stop = true;
 
-            ThreadPool.QueueUserWorkItem(MainThread);
-
-
-
-            //Thread t = new Thread(MainThread);
-            //t.Name = "Test Thread";
-            //t.IsBackground = true;
-            //t.Start();
-
-            //Console.WriteLine("Waiting for Thread!");
-
-            //t.Join();
-            //Console.WriteLine("Hello, World!");
-            while(true)
-            {
-
-            }
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+            t.Wait();
+            Console.WriteLine("종료 성공");
         }
     }
 }
