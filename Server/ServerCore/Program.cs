8be58f7ed7b87bdc7b55aa84d46036e587f3bd38
JiHyeok Heo <vndj1212@gmail.com>
2024-01-30 22:20:18 +0900

    SpinLock v2

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -7,40 +7,40 @@ namespace ServerCore
 {
     class Program
     {
-        static int _num = 0;
-        static Mutex _lock = new Mutex(); // 커널 동기화
+        // a. 근성 -> 스핀락은 무한히 시도하(a번)다가 답이 없으면 양보(b번)를 시도하는식으로 구현되어있다.
+        // b. 양보
+        // c. 갑질
 
-        static void Thread_1()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
-        }
+        // 상호배제
+        // Monitor 
+        static object _lock = new object();
+        static SpinLock _lock2 = new SpinLock();
+        // 직접 만든다.
 
-        static void Thread_2()
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
-        }
-
+        // 게임 개발하는데에 있어 필요없음
+        static Mutex _lock3 = new Mutex();  // 커널모드를 사용하기에 많이 무겁다 // 별도의 프로그램끼리도 동기화 작업을 할때 사용
+        
+        // 컨텐츠 구현 시
+        // 심리스 RPG를 만들때는 멀티스레드가 필요로 하다 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
+            lock(_lock)
+            {
 
-            t1.Start();
-            t2.Start();
+            }
+            
+            // 스핀락 사용
+            //bool lockTaken = false;
+            //try
+            //{
+            //    _lock2.Enter(ref lockTaken);
+            //}
+            //finally
+            //{
+            //    if(lockTaken)
+            //        _lock2.Exit();
+            //}
 
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(_num);
         }
     }
 }
