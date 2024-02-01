75f6e8550674ddc5f9bdbae9f13ffe3f7b920731
JiHyeok Heo <vndj1212@gmail.com>
2024-02-01 21:08:17 +0900

    Thread Local Storage

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -5,39 +5,29 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    // [ JobQueue ] // TLS 공간에 한방에 많이 뽑아다가 가져다가 쓰면 된다
+
     class Program
     {
-        static volatile int count = 0;
-        static Lock _lock = new Lock();
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My name Is {Thread.CurrentThread.ManagedThreadId}"; });
+
+        static void WhoAmI()
+        {
+            bool repeat = ThreadName.IsValueCreated;
+            if (repeat)
+                Console.WriteLine(ThreadName.Value + "(repeat)");
+            else
+                Console.WriteLine(ThreadName.Value);
+
+        }
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(delegate ()
-            {
-                for (int i = 0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    count++;
-                    _lock.WriteUnlock();
-                }
-            });
-            Task t2 = new Task(delegate ()
-            {
-                for (int i = 0; i < 100000; i++)
-                {
-                    _lock.WriteLock();
-                    count--;
-                    _lock.WriteUnlock();
-                }
-            });
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
 
-            t1.Start();
-            t2.Start();
-
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(count);
-
+            ThreadName.Dispose();
         }
     }
 }
