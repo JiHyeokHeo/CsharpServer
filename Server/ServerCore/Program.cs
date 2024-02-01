4cda5e5d91c9cb38915d44f44f99a3e44f3a307e
JiHyeok Heo <vndj1212@gmail.com>
2024-02-01 19:02:01 +0900

    ReadWriteLock 실습

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -7,52 +7,36 @@ namespace ServerCore
 {
     class Program
     {
-        // a. 근성 -> 스핀락은 무한히 시도하(a번)다가 답이 없으면 양보(b번)를 시도하는식으로 구현되어있다.
-        // b. 양보
-        // c. 갑질
-
-        // 상호배제
-        // Monitor 
-        static object _lock = new object();
-        static SpinLock _lock2 = new SpinLock();
-        // RWLock ReaderWriteLock
-        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
-
-
-        // 직접 만든다.
-
-        // 운영 보상이 주에 1번만 할텐데 lock을 하면 아쉽다.
-        // [] [] [] [] []
-        class Reward
-        {
-
-        }
-
-
-        // 99.999 
-        static Reward GetRewardById(int id)
-        {
-            _lock3.EnterReadLock();
-
-            _lock3.ExitReadLock();
-            return null;
-        }
-
-        // 0.001%
-        static void AddReward(Reward reward)
-        {
-            _lock3.EnterWriteLock();
-
-            _lock3.ExitWriteLock();
-        }
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
 
         static void Main(string[] args)
         {
-            lock(_lock)
+            Task t1 = new Task(delegate ()
             {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
 
-            }
-            
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(count);
 
         }
     }
